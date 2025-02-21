85903b68a00db93c1ab1bc3afdfc5781ad37123a
zaqamar <qamarozzaman@gmail.com>
2025-02-21 08:27:55 -0500

    write index.js file

--- final_project/index.js
+++ final_project/index.js
@@ -12,6 +12,24 @@ app.use("/customer",session({secret:"fingerprint_customer",resave: true, saveUni
 
 app.use("/customer/auth/*", function auth(req,res,next){
 //Write the authenication mechanism here
+ const username=req.body.username;
+    const password=req.body.password;
+    if(!username||!password){
+        return res.status(400).json({message: "Invalid username and password!"});
+    }
+
+    if(authenticated.authenticatedUser(username,password)){
+        let accessToken=jwt.sign({
+            username:username
+        }, 'access',{expiresIn:60*60});
+        req.session.authorization={
+            accessToken,username
+        }
+        return next();
+    }
+    else{     
+        return res.status(400).json({message: "Invalid username and password!"});
+    }
 });
  
 const PORT =5000;
